548b18fba23f03a7705149d99b18f39b4eb75ce8
fabb <milkservice@gmail.com>
2012-03-21 12:00:54 +0100

    template files for future assignments

---
+++ AufgabeFFPX_Template.hs
@@ -0,0 +1,13 @@
+module AufgabeFFP1 where
+
+-- Assignment 1.1
+{-
+Assignment 1.1 description
+-}
+
+-- function description
+f :: Integer -> [Integer]
+
+f a 
+  | a == 0 = [0]
+  | otherwise = [a]
---
+++ AufgabeFFPX_Test_Template.hs
@@ -0,0 +1,41 @@
+module AufgabeFFP1_Test where
+
+--http://leiffrenzel.de/papers/getting-started-with-hunit.html
+
+import Test.HUnit
+import AufgabeFFP1
+
+
+cases1 = TestLabel "Assignment 1.1" $ TestList [
+
+  TestCase $ assertEqual "Fail 1" (True) (False)
+  ,
+  TestCase $ assertEqual "Fail 2" (False) (True)
+  
+  ]
+
+
+cases2 = TestLabel "Assignment 1.2" $ TestList [
+
+  
+  ]
+
+
+cases3 = TestLabel "Assignment 1.3" $ TestList [
+
+  
+  ]
+
+
+cases4 = TestLabel "Assignment 1.4" $ TestList [
+
+  
+  ]
+
+
+cases5 = TestLabel "Assignment 1.5" $ TestList [
+
+    
+  ]
+
+main = runTestTT $ TestList [cases1, cases2, cases3, cases4, cases5]
