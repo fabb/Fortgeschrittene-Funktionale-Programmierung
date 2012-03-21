400873ddd824caf568b2a8f010fd32f410afc5c6
fabb <milkservice@gmail.com>
2012-03-21 22:15:55 +0100

    dummy implementation of assignment 1

--- AufgabeFFP1.hs
+++ AufgabeFFP1.hs
@@ -2,12 +2,89 @@ module AufgabeFFP1 where
 
 -- Assignment 1.1
 {-
-Assignment 1.1 description
+Schreiben Sie eine 0-stellige Haskell-Rechenvorschrift pof2s :: [Integer], die
+den Strom der 2er-Potenzen liefert, d.h. den Strom [1,2,4,8,16,32,....
+Versuchen Sie, pof2s rekursiv ausschließlich mit Hilfe von map (sowie dem Li-
+stenkonstruktor “:” und arithmetischen Funktionen zu definieren).
 -}
 
 -- function description
-f :: Integer -> [Integer]
+pof2s :: [Integer]
+pof2s = undefined
 
-f a 
-  | a == 0 = [0]
-  | otherwise = [a]
+
+-- Assignment 1.2
+{-
+Im folgenden ist ein Anfangsstück des Pascalschen Dreiecks dargestellt:
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+1 5 10 10 5 1
+...
+Wir bezeichnen die Zeilen dieses Dreiecks als Pascaltupel.
+Schreiben Sie eine 0-stellige Haskell-Rechenvorschrift pd :: [[Integer]], die
+den Strom der Pascaltupel liefert, d.h. den Strom [[1],[1,1],[1,2,1],[1,3,3,1],....
+Versuchen Sie, pd rekursiv ausschließlich mit Hilfe von zipWith (sowie den
+Standardkonstruktoren und -operatoren auf Listen und arithmetischen Funk-
+tionen zu definieren).
+-}
+
+pd :: [[Integer]]
+pd = undefined
+
+
+-- Assignment 1.3
+{-
+ie folgende Abbildung veranschaulicht, dass das Pascalsche Dreieck implizit
+die Folge der Fibonacci-Zahlen enthält.
+
+Schreiben Sie eine Haskell-Rechenvorschrift fibdiag
+fibdiag :: Integer -> [Integer]
+vor, die angewendet auf ein positives Argument n, n > 0, die Liste der in
+Pfeilrichtung angeordneten Summanden liefert, die sich zur n-ten Fibonacci-
+Zahl summieren.
+Folgende Beispiele zeigen das Aufruf/Resultatverhalten von fibdiag:
+fibdiag 1 ->> [1]
+fibdiag 2 ->> [1]
+fibdiag 3 ->> [1,1]
+fibdiag 5 ->> [1,3,1]
+fibdiag 8 ->> [1,6,10,4]
+-}
+
+fibdiag :: Integer -> [Integer]
+fibdiag = undefined
+
+
+-- Assignment 1.4
+{-
+ Schreiben Sie mithilfe von fibdiag eine Haskell-Rechenvorschrift fibdiags,
+die den Strom der Diagonalen aus der obigen Abbildung liefert:
+fibdiags :: Integer -> [[Integer]]
+-}
+
+fibdiags :: [[Integer]]
+fibdiags = undefined
+
+
+-- Assignment 1.5
+{-
+Geben Sie weiters eine Haskell-Rechenvorschrift fibspd an, die den Strom der
+Fibonacci-Zahlen in der durch die Abbildung nahegelegten Weise aus dem Pas-
+calschen Dreieck generiert.
+fibspd :: [Integer]
+Die Implementierung des Stroms fibspd soll sich dabei geeignet auf den Strom
+pd (oder auf fibdiags) aus den vorherigen Teilaufgaben abstützen.
+Die Generator/Selektor-Aufrufe sollen folgende Resultate liefern:
+take 5 pd
+->>
+[ [1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1] ]
+take 5 fibdiags ->> [ [1], [1], [1,1], [1,2], [1,3,1] ]
+take 5 fibspd
+->>
+[1,1,2,3,5]
+-}
+
+fibspd :: [Integer]
+fibspd = undefined
--- AufgabeFFP1_Test.hs
+++ AufgabeFFP1_Test.hs
@@ -8,33 +8,45 @@ import AufgabeFFP1
 
 cases1 = TestLabel "Assignment 1.1" $ TestList [
 
-  TestCase $ assertEqual "Fail 1" (True) (False)
-  ,
-  TestCase $ assertEqual "Fail 2" (False) (True)
+  TestCase $ assertEqual "pof2s: first 7 not equal" (take 7 pof2s) ([1,2,4,8,16,32,64])
   
   ]
 
 
 cases2 = TestLabel "Assignment 1.2" $ TestList [
 
-  
+  TestCase $ assertEqual "pd: first ones not equal" (take 5 pd) ([ [1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1] ])
+
   ]
 
 
 cases3 = TestLabel "Assignment 1.3" $ TestList [
 
+  TestCase $ assertEqual "fibdiag 1" (fibdiag 1) ([1])
+  ,
+  TestCase $ assertEqual "fibdiag 2" (fibdiag 2) ([1])
+  ,
+  TestCase $ assertEqual "fibdiag 3" (fibdiag 3) ([1,1])
+  ,
+  TestCase $ assertEqual "fibdiag 4" (fibdiag 4) ([1,2])
+  ,
+  TestCase $ assertEqual "fibdiag 5" (fibdiag 5) ([1,3,1])
+  ,
+  TestCase $ assertEqual "fibdiag 8" (fibdiag 5) ([1,6,10,4])
   
   ]
 
 
 cases4 = TestLabel "Assignment 1.4" $ TestList [
 
+  TestCase $ assertEqual "fibdiags: first ones not equal" (take 5 fibdiags) ([ [1], [1], [1,1], [1,2], [1,3,1] ])
   
   ]
 
 
 cases5 = TestLabel "Assignment 1.5" $ TestList [
 
+  TestCase $ assertEqual "fibspd: first ones not equal" (take 5 fibspd) ([1,1,2,3,5])
     
   ]
 
