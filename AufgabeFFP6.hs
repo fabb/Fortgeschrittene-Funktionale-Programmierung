c3c5aaa02435cfba08c808e3c8388cf3bed9aadd
fabb <milkservice@gmail.com>
2012-05-09 22:44:56 +0200

    stubs for assignment 6

---
+++ AufgabeFFP6.hs
@@ -0,0 +1,120 @@
+module AufgabeFFP6 where
+
+import Data.List
+import Data.Ix
+--import Array
+import Data.Array.IArray
+
+
+-- Assignment 6.1
+{-
+Sei f = (f1,...,fk), k = 2, eine nichtleere endliche Folge ganzer Zahlen und
+g = (g1,...,gk-1) eine um 1 kürzere Folge der arithmetischen Operatoren +, *, -
+und / mit / ganzzahlige Division mit Rest.
+Eine Auswertung von f bzgl. g ist der Wert des Ausdrucks
+f1g1f2g2 ... fk-1gk-1fk
+ohne Beachtung der Regel Punkt- vor Strichrechnung.
+Beispiele:
+f = (3,5,2,(-2),7,0) hat bzgl. g = (+,/,*,+,-) den Wert (3+5/2*(-2)+7-0) =
+(-1); bzgl. g'= (*,-,+,/,+) den Wert (3 * 5 - 2 + (-2)/7 + 0) = 1.
+f = (4,2,3,(-4),5,2) hat bzgl. g = (+,/,*,+,-) den Wert (4+2/3*(-4)+5-2) =
+(-5); bzgl. g'= (*,-,+,/,+) den Wert (4 * 2 - 3 + (-4)/5 + 2) = 2.
+
+Schreiben Sie eine Haskell-Rechenvorschrift eval :: Array Int Int -> Array
+Int (Int->Int->Int) -> Int, die angewendet auf eine Zahlenfolge f und eine
+Operatorfolge g passender Länge den Wert von f bzgl. g im Sinne des vorste-
+hend eingeführten Auswertungsbegriffs bestimmt. Divisionen durch 0 führen
+dabei zu einem Fehlerabbruch der Auswertung.
+Beispiele:
+eval (array (1,3) [(1,1),(2,2),(3,3)]) (array (1,2) [(1,(+)),(2,(-))])
+->> 0
+eval (array (1,3) [(1,1),(2,2),(3,3)]) (array (1,2) [(1,(*)),(2,(+))])
+->> 5
+eval (array (1,3) [(1,1),(2,2),(3,3)]) (array (1,2) [(1,(-)),(2,(*))])
+->> (-3)
+Die Funktion eval wird nur auf zueinander passende Zahlen- und Operations-
+folgen angewendet.
+-}
+
+eval :: Array Int Int -> Array Int (Int->Int->Int) -> Int
+eval = undefined
+
+
+-- Assignment 6.2
+{-
+Die Funktion yield :: Array Int Int -> Int -> [Array Int (Int->Int->Int)]
+soll angewendet auf eine Zahlenfolge f und einen Zielwert w eine Liste derjeni-
+gen Folgen arithmetischer Operationen liefern, so dass die Auswertung der Ar-
+gumentliste bzgl. dieser Operationsfolge den Zielwert w ergibt. Die Reihenfolge
+verschiedener Operationsfolgen innerhalb der Ergebnisliste ist dabei unerheb-
+lich.
+Beispiele:
+yield array (1,3) [(1,1),(2,2),(3,3)] 6
+->> [array (1,2) (1,(+)),(2,(+)),array (1,2) (1,(*)),(2,(*))]
+yield array (1,3) [(1,1),(2,2),(3,3)] 4
+->> []
+yield array (1,3) [(1,1),(2,2),(3,3)] 0
+->> [array (1,2) (1,(+)),(2,(-)),array (1,2) (1,(/)),(2,(*)),
+array (1,2) (1,(/)),(2,(/))]
+Implementieren Sie zwei unterschiedliche Varianten
+– yield bt :: Array Int Int -> Int -> [Array Int (Int->Int->Int)]
+– yield gtf :: Array Int Int -> Int -> [Array Int (Int->Int->Int)]
+die funktional äquivalent zur Funktion yield sind, wobei yield bt sich auf das
+Backtracking-Funktional aus Kapitel 3.2 aus der Vorlesung und yield gtf sich
+auf das generate/transform/filter-Prinzip abstützt.
+Implementieren Sie für yield gtf drei Funktionen generate,
+transform und filt, so dass sich yield gtf als sequentielle Komposition
+filt . transform . generate
+ergibt. Die Funktion generate erzeugt alle Operationsfolgen (passender Länge),
+transform übernimmt die Auswertung, filt wählt die Auswertungen mit pas-
+sendem Zielwert aus.
+(Hinweis: Eine unmittelbare Ausgabe der Resultate ist nicht möglich, da der
+Resultattyp von yield bt bzw. yield gtf nicht in der Klasse Show liegt. Beide
+Funktionen werden nur mit mindestens zweielementigen Zahlenfolgen aufgeru-
+fen.)
+-}
+
+-- TODO noch nicht abgesegnet
+data ArithOp = Plus | Minus | Times | Div
+
+conv :: ArithOp -> (Int->Int->Int)
+conv Plus = (+)
+conv Minus = (-)
+conv Times = (*)
+conv Div = div
+
+bla a = amap conv a
+
+
+yield :: Array Int Int -> Int -> [Array Int (Int->Int->Int)]
+yield = undefined
+
+
+yield_bt :: Array Int Int -> Int -> [Array Int (Int->Int->Int)]
+yield_bt = undefined
+
+
+yield_gtf :: Array Int Int -> Int -> [Array Int (Int->Int->Int)]
+yield_gtf = filt . transform . generate
+
+filt = undefined
+transform = undefined
+generate = undefined
+
+
+-- Assignment 6.3
+{-
+Machen Sie den Datentyp Array Int (Int->Int->Int) zu einer Instanz der
+Klasse Show. Die arithmetischen Operationen aus den vorigen Aufgaben sollen
+als Zeichenreihen
+"plus", "minus", "times", "div"
+dargestellt werden. Für mögliche andere Operationen wird keine besondere Aus-
+gabe verlangt.
+-}
+
+{-
+instance Show (Array Int (Int->Int->Int)) where
+	show = undefined
+-}
+
+	
---
+++ AufgabeFFP6_Test.hs
@@ -0,0 +1,57 @@
+module AufgabeFFP6_Test where
+
+--http://leiffrenzel.de/papers/getting-started-with-hunit.html
+
+import Test.HUnit
+import AufgabeFFP6
+
+
+import Data.List
+import Data.Ix
+import Array
+
+
+cases1 = TestLabel "Assignment 6.1" $ TestList [
+
+  TestCase $ assertEqual "eval" (0) (eval (array (1,3) [(1,1),(2,2),(3,3)]) (array (1,2) [(1,(+)),(2,(-))]))
+  ,
+  TestCase $ assertEqual "eval" (5) (eval (array (1,3) [(1,1),(2,2),(3,3)]) (array (1,2) [(1,(*)),(2,(+))]))
+  ,
+  TestCase $ assertEqual "eval" (-3) (eval (array (1,3) [(1,1),(2,2),(3,3)]) (array (1,2) [(1,(-)),(2,(*))]))
+  
+  ]
+
+
+cases2 = TestLabel "Assignment 6.2" $ TestList [
+
+  TestCase $ assertEqual "yield" ([array (1,2) [(1,(+)),(2,(+))], array (1,2) [(1,(*)),(2,(*))]]) (yield (array (1,3) [(1,1),(2,2),(3,3)]) 6)
+  ,
+  TestCase $ assertEqual "yield" ([]) (yield (array (1,3) [(1,1),(2,2),(3,3)]) 4)
+  ,
+  TestCase $ assertEqual "yield" ([array (1,2) [(1,(+)),(2,(-))], array (1,2) [(1,(/)),(2,(*))], array (1,2) [(1,(/)),(2,(/))]]) (yield (array (1,3) [(1,1),(2,2),(3,3)]) 0)
+  ,
+  TestCase $ assertEqual "yield_bt" (yield (array (1,3) [(1,1),(2,2),(3,3)]) 6) (yield_bt (array (1,3) [(1,1),(2,2),(3,3)]) 6)
+  ,
+  TestCase $ assertEqual "yield_bt" (yield (array (1,3) [(1,1),(2,2),(3,3)]) 4) (yield_bt (array (1,3) [(1,1),(2,2),(3,3)]) 4)
+  ,
+  TestCase $ assertEqual "yield_bt" (yield (array (1,3) [(1,1),(2,2),(3,3)]) 0) (yield_bt (array (1,3) [(1,1),(2,2),(3,3)]) 0)
+  ,
+  TestCase $ assertEqual "yield_gtf" (yield (array (1,3) [(1,1),(2,2),(3,3)]) 6) (yield_gtf (array (1,3) [(1,1),(2,2),(3,3)]) 6)
+  ,
+  TestCase $ assertEqual "yield_gtf" (yield (array (1,3) [(1,1),(2,2),(3,3)]) 4) (yield_gtf (array (1,3) [(1,1),(2,2),(3,3)]) 4)
+  ,
+  TestCase $ assertEqual "yield_gtf" (yield (array (1,3) [(1,1),(2,2),(3,3)]) 0) (yield_gtf (array (1,3) [(1,1),(2,2),(3,3)]) 0)
+  
+  ]
+
+
+cases3 = TestLabel "Assignment 6.3" $ TestList [
+
+  TestCase $ assertEqual "Fail 1" (True) (False)
+  ,
+  TestCase $ assertEqual "Fail 2" (False) (True)
+  
+  ]
+
+
+main = runTestTT $ TestList [cases1, cases2, cases3]
